9b91c4241e479903edef945e46f498e3d1178b48
Sujal Rajesh Kadbe <sujalkadbe007@gmail.com>
2025-12-03 12:00:59 +0530

    Linear Regression.py

--- .py
+++ .py
@@ -4,3 +4,15 @@ from sklearn.linear_model import LinearRegression
 from sk.learn.mrtrics import mean_squared_error
 
 df = pd.read_csv("house_prices.csv")
+
+X = df.drop("price", axis = 1)
+y = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42)
+
+model + LinearRegresion()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, pred))
